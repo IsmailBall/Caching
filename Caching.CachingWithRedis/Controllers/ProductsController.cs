b483215a715bd853abb46afd7c19aad7c9c6af40
sancar bal <sncr.bl@hotmail.com>
2022-06-23 00:30:37 +0300

    Add project files.

---
+++ Caching.CachingWithRedis/Controllers/ProductsController.cs
@@ -0,0 +1,85 @@
+﻿using Caching.CachingWithRedis.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Caching.CachingWithRedis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet("SetValue")]
+        public async Task<IActionResult> SetCache()
+        {
+            string name = "ismail";
+            DistributedCacheEntryOptions distributedCacheEntryOptions = new DistributedCacheEntryOptions();
+            distributedCacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(2);
+
+            await _distributedCache.SetStringAsync(nameof(name), name, distributedCacheEntryOptions);
+
+            return Ok("Okey");
+        }
+
+        [HttpGet("GetValue")]
+        public async Task<IActionResult> GetCache()
+        {
+            string name = "ismail";
+
+            name = await _distributedCache.GetStringAsync(nameof(name));
+
+            return Ok(name);
+        }
+        [HttpGet("SetCacheWithComplexType")]
+        public async Task<IActionResult> SetCacheWithComplexType()
+        {
+            Product product = new Product() { Id = 1, Name = "Book", Stock = 1000 };
+            
+            DistributedCacheEntryOptions distributedCacheEntryOptions = new DistributedCacheEntryOptions();
+            distributedCacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(2);
+
+            string convertedObject = JsonSerializer.Serialize(product);
+
+            await _distributedCache.SetStringAsync("product:1", convertedObject,distributedCacheEntryOptions);
+
+            return Ok("Okey");
+        }
+
+        [HttpGet("GetCacheWithComplexType")]
+        public async Task<IActionResult> GetCacheWithComplexType()
+        {
+            var productJson = await _distributedCache.GetStringAsync("product:1");
+            var product = JsonSerializer.Deserialize<Product>(productJson);
+
+            return Ok(product!.Name + " " + product.Id + " " + product.Stock);
+        }
+
+        [HttpGet("SetCacheWithFile")]
+        public async Task<IActionResult> SetCacheWithFile()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\img\ınterstller.jpg");
+            var file = await System.IO.File.ReadAllBytesAsync(path);
+
+            await _distributedCache.SetAsync("file", file);
+
+            return Ok("Okey");
+        }
+
+        [HttpGet("GetCacheWithFile")]
+        public async Task<IActionResult> GetCacheWithFile()
+        {
+            var fileByte = await _distributedCache.GetAsync("file");
+
+            return File(fileByte, "image/jpg");
+        }
+    }
+}
---
+++ InMemoryCacheApp.API/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCacheApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private IMemoryCache _memoryCache;
+
+        public HomeController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
+            MemoryCacheEntryOptions memoryCacheEntryOptions = new();
+
+            memoryCacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
+            memoryCacheEntryOptions.SlidingExpiration = TimeSpan.FromSeconds(2);
+
+            memoryCacheEntryOptions.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _memoryCache.Set<string>("callback", $"key => {key}:  value: {value} : reason: {reason}");
+            });
+
+            _memoryCache.Set<string>("time", DateTime.Now.ToString(),memoryCacheEntryOptions);
+            return Ok();
+        }
+        [HttpGet("GetResult")]
+        public IActionResult GetResult()
+        {
+
+            
+            var result = _memoryCache.GetOrCreate("time", (entry) =>
+            {
+                var value = DateTime.Now.ToString();
+                entry.SetValue(value);
+                return value;
+            });
+            
+
+            var data = _memoryCache.Get<string>("callback");
+            
+            return Ok(data);
+        }
+    }
+}
